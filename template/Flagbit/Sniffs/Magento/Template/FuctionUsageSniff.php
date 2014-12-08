3a747be1e07b5aad0a95681ddd56c8d325d02863
xpoback <xpoback@gmail.com>
2014-12-08 17:31:44 +0100

    Add sniff for checking functions in templates

---
+++ template/Flagbit/Sniffs/Magento/Template/FuctionUsageSniff.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Class Flagbit_Sniffs_Magento_Template_FuctionUsageSniff
+ */
+class Flagbit_Sniffs_Magento_Template_FuctionUsageSniff implements PHP_CodeSniffer_Sniff
+{
+
+    protected $_allowedFunctions = array(
+        'count',
+    );
+
+    public function register()
+    {
+        return array(T_STRING);
+    }
+
+
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
+
+
+        $prevPtr = $phpcsFile->findPrevious(array(T_WHITESPACE), $stackPtr - 1, null, true);
+        $nextPtr = $phpcsFile->findPrevious(array(T_WHITESPACE), $stackPtr + 1, null, true);
+
+        $isClass = $tokens[$nextPtr]['code'] === T_DOUBLE_COLON;
+        $isConstant = strtoupper($tokens[$stackPtr]['content']) === $tokens[$stackPtr]['content'];
+        $isMethod = in_array($tokens[$prevPtr]['code'], array(T_DOUBLE_COLON, T_OBJECT_OPERATOR));
+        $isAllowed = in_array($tokens[$stackPtr]['content'], $this->_allowedFunctions);
+
+        if (!$isClass
+            && !$isConstant
+            && !$isMethod
+            && !$isAllowed
+        ) {
+            $phpcsFile->addError(
+                'Usage of the function "' . $tokens[$stackPtr]['content'] . '()" is not allowed in templates',
+                $stackPtr
+            );
+        }
+
+    }
+
+
+}
